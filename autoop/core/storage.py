40e931a2f864a79af7aa438c6a6406f970b86864
Niels ten Berg <niels@tenberg.com>
2024-11-05 21:34:13 +0100

    Flake8 Fixes

--- autoop/core/storage.py
+++ autoop/core/storage.py
@@ -62,13 +62,21 @@ class Storage(ABC):
 
 
 class LocalStorage(Storage):
-
+    """
+    Storage implementation for handling files locally.
+    """
     def __init__(self, base_path: str = "./assets"):
+        """
+        Initialize the local storage with a base directory.
+        """
         self._base_path = base_path
         if not os.path.exists(self._base_path):
             os.makedirs(self._base_path)
 
     def save(self, data: bytes, key: str):
+        """
+        Save data to the specified file path.
+        """
         path = self._join_path(key)
         if not os.path.exists(path):
             os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -76,25 +84,40 @@ class LocalStorage(Storage):
             f.write(data)
 
     def load(self, key: str) -> bytes:
+        """
+        Load data from the specified file path.
+        """
         path = self._join_path(key)
         self._assert_path_exists(path)
         with open(path, 'rb') as f:
             return f.read()
 
     def delete(self, key: str = "/"):
+        """
+        Delete data at the specified file path.
+        """
         self._assert_path_exists(self._join_path(key))
         path = self._join_path(key)
         os.remove(path)
 
     def list(self, prefix: str) -> List[str]:
+        """
+        List all file paths under the specified directory.
+        """
         path = self._join_path(prefix)
         self._assert_path_exists(path)
         keys = glob(path + "/**/*", recursive=True)
         return list(filter(os.path.isfile, keys))
 
     def _assert_path_exists(self, path: str):
+        """
+        Ensure the specified path exists, raising an error if not.
+        """
         if not os.path.exists(path):
             raise NotFoundError(path)
 
     def _join_path(self, path: str) -> str:
+        """
+        Construct a full path from the base path and a relative path.
+        """
         return os.path.join(self._base_path, path)
